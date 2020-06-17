e03b97c8a815bbee79a454f6e041e7761a46b7fb
Jon <59712501+jmillermedia@users.noreply.github.com>
2020-06-17 09:47:21 -0400

    Success
    I was able to get an image to go from one div to the other, though the code doesn't have multiple images and multiple drop zones. Will try that next.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 // JAVASCRIPT START
+
+(() => {
+    const image = document.querySelector('#sticks'),
+    dropZone = document.querySelectorAll('.dropZone');
+
+    function allowDrag(event) {
+        event.dataTransfer.setData('draggedImage', this.id);
+    }
+
+    function allowDragOver(event) {
+		//console.log('Dragged something over me!');
+		event.preventDefault();
+    }
+
+    function allowDrop(event) {
+        event.preventDefault();
+        let droppedImage = event.dataTransfer.getData('draggedImage');
+
+            this.appendChild(image); 
+    }
+
+
+    dropZone.forEach(zone => {
+		zone.addEventListener('dragover', allowDragOver);
+		zone.addEventListener('drop', allowDrop);
+	});
+
+})();
